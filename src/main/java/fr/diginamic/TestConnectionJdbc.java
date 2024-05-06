c380e45defae001fc953d4fed04106dca0742802
Cyril <cyril.personne@outlook.fr>
2024-05-06 11:04:26 +0200

    push

--- src/main/java/fr/diginamic/TestConnectionJdbc.java
+++ src/main/java/fr/diginamic/TestConnectionJdbc.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class TestConnectionJdbc {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo-jdbc","root", "root");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/compta","root", "root");
         System.out.println(connection);
         connection.close();
     }
