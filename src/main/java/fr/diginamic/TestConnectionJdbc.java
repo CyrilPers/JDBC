3d1be3d9cc1f4e097f204dd0cb01a00d8f68fd46
Cyril <cyril.personne@outlook.fr>
2024-05-06 12:54:49 +0200

    push

--- src/main/java/fr/diginamic/TestConnectionJdbc.java
+++ src/main/java/fr/diginamic/TestConnectionJdbc.java
@@ -3,11 +3,25 @@ package fr.diginamic;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnectionJdbc {
-    public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/compta","root", "root");
-        System.out.println(connection);
-        connection.close();
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PWD;
+
+    static {
+        ResourceBundle dbConfig = ResourceBundle.getBundle("db");
+        DB_URL = dbConfig.getString("db.url");
+        DB_USER = dbConfig.getString("db.user");
+        DB_PWD = dbConfig.getString("db.password");
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD)) {
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
